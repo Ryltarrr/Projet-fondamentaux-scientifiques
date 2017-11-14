230efabe4d510c1101409b5dd48af68bea4c1b7a
bloopi <bloopi@users.noreply.github.com>
2017-11-14 09:10:35 +0100

    sensor_pulse

--- Arduino/pulse_sensor/pulse_sensor.ino
+++ Arduino/pulse_sensor/pulse_sensor.ino
@@ -1,29 +1,31 @@
-int valeurPrecedente = 0;
-long tempsPrecedent = 0;
+unsigned long time;
+
 
 void setup() {
+
   Serial.begin(9600);
+
+  Serial.println("Veuillez patienter");
+
+  time = millis();
+
+  // un petit réchauffement du convertisseur
+  // analogique-numérique semble améliorer les résultats
+  while((millis()-time)<1000){
+    analogRead(0);
+  }
+
+  time = millis();
+
+  // on affiche au moniteur série pendant 5 secondes
+  while((millis()-time)< 5000){
+    Serial.println(((1000.0*60.0)/(analogRead(0)))-100);
+  }
+
 }
 
 
 void loop() {
 
-  int valeurActuelle, valeurSeuil;
-  long tempsDetection;
-
-  valeurActuelle = analogRead(0);
-  valeurSeuil = 650;
-
-  if (valeurActuelle > valeurSeuil) {  // on est dans la zone max
-    if (valeurPrecedente <= valeurSeuil) {  // est-ce qu'on vient d'y entrer?
-      tempsDetection = millis();
-      if (tempsDetection > (tempsPrecedent + 200)){  // ce n'est pas seulement du bruit?
-        Serial.println( (1000.0 * 60.0) / (tempsDetection - tempsPrecedent),0);
-        tempsPrecedent = tempsDetection;
-      }
-    }
-  }
-
-  valeurPrecedente = valeurActuelle;
 
 }
