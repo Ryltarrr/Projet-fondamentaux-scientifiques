bc37752446c06f2bb4338d400ccaf381a205b753
LoicMoerman <loic.moerman@laposte.net>
2017-11-14 10:18:31 +0100

    arduino

---
+++ Arduino/Programme Arduino/main.ino
@@ -0,0 +1,76 @@
+/*#include "param.h" */
+
+
+const int L1 = 2;
+const int L2 = 3;
+const int L3 = 4;
+const int L4 = 5;
+const int L5 = 6;
+const int L6 = 7;
+const int L7 = 8;
+const int L8 = 9;
+const int L9 = 10;
+const int L10 = 11;
+
+const int del = 30;
+const int dellow = 30;
+
+const int pouls = 70;
+
+
+
+void setup() {
+
+pinMode(L1,OUTPUT);
+pinMode(L2,OUTPUT);
+pinMode(L3,OUTPUT);
+pinMode(L4,OUTPUT);
+pinMode(L5,OUTPUT);
+pinMode(L6,OUTPUT);
+pinMode(L7,OUTPUT);
+pinMode(L8,OUTPUT);
+pinMode(L9,OUTPUT);
+pinMode(L10,OUTPUT);
+
+}
+
+void loop() {
+CoeurAllume
+
+
+void CoeurAllume (int pouls){
+digitalWrite(L1,HIGH);
+digitalWrite(L2,HIGH);
+digitalWrite(L3,HIGH);
+digitalWrite(L4,HIGH);
+digitalWrite(L5,HIGH);
+digitalWrite(L6,HIGH);
+digitalWrite(L7,HIGH);
+digitalWrite(L8,HIGH);
+digitalWrite(L9,HIGH);
+digitalWrite(L10,HIGH);
+delay(70);
+digitalWrite(L1,LOW);
+digitalWrite(L2,LOW);
+digitalWrite(L3,LOW);
+digitalWrite(L4,LOW);
+digitalWrite(L5,LOW);
+digitalWrite(L6,LOW);
+digitalWrite(L7,LOW);
+digitalWrite(L8,LOW);
+digitalWrite(L9,LOW);
+digitalWrite(L10,LOW);
+    
+
+
+
+  
+}
+
+
+
+
+
+
+
+
