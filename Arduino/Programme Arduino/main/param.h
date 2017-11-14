ad35825963282bbd9f261f14adcb744da06143f5
JulesPigeau <julespigeau0@gmail.com>
2017-11-14 10:25:06 +0100

    ez

---
+++ Arduino/Programme Arduino/main/param.h
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+
+int CoeurAllume();
+int OneOnTwo();
+int OneOnThree();
+int ChooseOne();
+int Caterpillar();
+int OtherIdea();
+
+void setTenLEDs(int val);
+void setOneOnTwo(int val);
+void setOneOnThree(int val);
+void setChooseOne(int val);
+void setCaterpillar(int val);
+void setOtherIdea(int val);
+
+void IgnitionLEDs();
+
+
+
+
+
