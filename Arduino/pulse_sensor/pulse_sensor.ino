4f7902aef1e7e9d2b47ad5846efe0e49436ca694
bloopi <bloopi@users.noreply.github.com>
2017-11-16 13:05:05 +0100

    pouls

--- Arduino/pulse_sensor/pulse_sensor.ino
+++ Arduino/pulse_sensor/pulse_sensor.ino
@@ -1,8 +1,9 @@
 unsigned long time;
+int n,i;
 
 
 void setup() {
-
+  n=10000;
   Serial.begin(9600);
 
   Serial.println("Veuillez patienter");
@@ -11,15 +12,17 @@ void setup() {
 
   // un petit réchauffement du convertisseur
   // analogique-numérique semble améliorer les résultats
-  while((millis()-time)<1000){
+  while((millis()-time)<1){
     analogRead(0);
   }
 
   time = millis();
 
+
   // on affiche au moniteur série pendant 5 secondes
-  while((millis()-time)< 5000){
+  for (i=0;i<n;i++){
     Serial.println(((1000.0*60.0)/(analogRead(0)))-100);
+    delay(1);
   }
 
 }
