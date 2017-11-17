a43d62e9627801378fc8480518f865e5d16b9f4e
Justin Guerré <guerrejustin@gmail.com>
2017-11-17 10:16:09 +0100

    Petites modifications

--- M4-traitement-des-donnees/sources/actions.c
+++ M4-traitement-des-donnees/sources/actions.c
@@ -177,7 +177,7 @@ void afficherMediane(Bpm *tab, int compteur) {
     tabCpy[i].freq = tab[i].freq;
   }
   quicksort(tabCpy, compteur);
-  printf("La mediane du pouls est : %f BPM, \x85 %d millisecondes.\n", tabCpy[compteur/2].freq, tabCpy[compteur/2].temps);
+  printf("La mediane du pouls est : %.2f BPM, \x85 %d millisecondes.\n", tabCpy[compteur/2].freq, tabCpy[compteur/2].temps);
 }
 
 
